f6f73547f578507957771a5c935ec614f5d73d61
Harsh <hjain_be21@thapar.edu>
2023-02-04 16:33:54 +0530

    structure defined

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import './pages/Home/Home'
+import Home from './pages/Home/Home';
 
 function App() {
   return (
     <>
-    
+    <Home/>
     </>
   );
 }
