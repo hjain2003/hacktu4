e5dbf3984f1246486db97b4e68beb365c344cfb2
Harsh <hjain_be21@thapar.edu>
2023-02-05 01:27:54 +0530

    added navbar,homepage

---
+++ client/src/components/Card/Card.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import '../Card/Card.css';
+import { FaPlayCircle } from "react-icons/fa";
+
+
+const Card = () => {
+  return (
+    <div id="card_wrapper_col">
+        <div id="img">Stocks</div>
+        <div id="card_info_row">
+            <h3>5<h6>Chapters</h6></h3>
+            <div id="line"></div>
+            <h3>12<h6>Topics</h6></h3>
+            <div id="circle_play"><FaPlayCircle size={70}/></div>
+        </div>
+    </div>
+  )
+}
+
+export default Card
